d2d33801857a92d811d75301ab3bd01aa26de08e
Omar Faruk <omarfaruk2y2@gmail.com>
2023-11-06 22:07:52 +0600

    project done

--- gallary/src/App.jsx
+++ gallary/src/App.jsx
@@ -3,13 +3,24 @@ import { ImImage } from 'react-icons/im';
 import Checkbox from '@mui/material/Checkbox';
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
-
 const App = (props) => {
 
   const dragItem = useRef();
   const dragOverItem = useRef();
 
-  const [list, setList] = useState(['https://i.ibb.co/M1T3fhN/image-1.webp', 'https://i.ibb.co/mcDSYDt/image-2.webp', 'https://i.ibb.co/XYSTvzH/image-3.webp','https://i.ibb.co/tQd7g3x/image-4.webp','https://i.ibb.co/JRhWFbK/image-5.webp','https://i.ibb.co/XSmBXXR/image-6.webp','https://i.ibb.co/RSWkYFW/image-7.webp','https://i.ibb.co/pWT7wvw/image-8.webp', 'https://i.ibb.co/xsyDwcm/image-9.webp', 'https://i.ibb.co/ZShS7mW/image-10.jpg', 'https://i.ibb.co/GdCKNYR/image-11.jpg']);
+  const [list, setList] = useState([
+    { id: 1, img: 'https://i.ibb.co/M1T3fhN/image-1.webp' },
+    { id: 2, img: 'https://i.ibb.co/mcDSYDt/image-2.webp' },
+    { id: 3, img: 'https://i.ibb.co/XYSTvzH/image-3.webp' },
+    { id: 4, img: 'https://i.ibb.co/tQd7g3x/image-4.webp' },
+    { id: 5, img: 'https://i.ibb.co/JRhWFbK/image-5.webp' },
+    { id: 6, img: 'https://i.ibb.co/XSmBXXR/image-6.webp' },
+    { id: 7, img: 'https://i.ibb.co/RSWkYFW/image-7.webp' },
+    { id: 8, img: 'https://i.ibb.co/pWT7wvw/image-8.webp' },
+    { id: 9, img: 'https://i.ibb.co/xsyDwcm/image-9.webp' },
+    { id: 10, img: 'https://i.ibb.co/ZShS7mW/image-10.jpg' },
+    { id: 11, img: 'https://i.ibb.co/GdCKNYR/image-11.jpg' }
+  ]);
 
   const dragStart = (e, position) =>{
     dragItem.current = position;
@@ -29,13 +40,40 @@ const App = (props) => {
     setList(copyListItems);
   }
 
-  const onDrop = (e) => {console.log(e.target);}
+  const [selectedItems, setSelectedItems] = useState([]);
+  console.log(selectedItems);
+  const handleItemSelection = (itemId) => {
+    const isSelected = selectedItems.includes(itemId);
+    if (isSelected) {
+      setSelectedItems(selectedItems.filter((id) => id !== itemId));
+    } else {
+      setSelectedItems([...selectedItems, itemId]);
+    }
+  };
+
+  const handleDeleteSelected = () => {
+    const updatedItems = list.filter((item) => !selectedItems.includes(item.id));
+    setList(updatedItems);
+    setSelectedItems([]);
+  };
 
   return (
     <>
-    <div className='max-w-7xl mx-auto my-6'>
+    <div className='max-w-7xl mx-auto my-6 px-3'>
       <div className='px-9 pt-9 pb-5 bg-white rounded-t-md border-b'>
-          <h1 className='text-2xl font-medium'>Gallery</h1>
+        {selectedItems.length > 0 ?
+          <div className='flex justify-between'>
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <input type="checkbox" checked="checked" readOnly className="checkbox checkbox-primary mb-2 mr-2" />
+                <span className="label-text text-xl font-medium"><span className='mx-2'>{selectedItems.length}</span>File Selected</span>
+              </label>
+            </div>
+            <button className='text-xl font-medium text-red-600' onClick={handleDeleteSelected}>Delete File</button>
+          </div>
+          : 
+          <h1 className='text-xl font-medium'>Gallery</h1>
+        }
       </div>
       <div className='container p-9 bg-white rounded-b-md'>
       {
@@ -44,32 +82,33 @@ const App = (props) => {
             onDragStart={(e) => dragStart(e, index)}
             onDragEnter={(e) => dragEnter(e, index)}
             onDragEnd = {drop}
-            onDrop={(e) => onDrop(e)} 
             key={index}
             draggable
             className="card cursor-move border relative"
           >
             <div className='overly'></div>
-            <img className='rounded-md h-full' src={item} alt="" />
+            <img className='rounded-md h-full' src={item.img} alt="" />
             <div className='check'>
-              <Checkbox className='bg-white text-white' {...label} />
+              <Checkbox 
+                  checked={selectedItems.includes(item.id)}
+                  onChange={() => handleItemSelection(item.id)} 
+                  className='bg-white border-white text-white relative' {...label} 
+              />
             </div>
-
           </div>
         )
       }
       <div className="flex items-center justify-center w-full">
-          <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+          <label htmlFor="dropzone-file" className="drop-file flex flex-col items-center justify-center w-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
             <div className="flex flex-col items-center justify-center pt-5 pb-6">
               <ImImage></ImImage>
-              <h1>Add images</h1>
+              <h1 className='mt-2'>Add images</h1>
             </div>
             <input id="dropzone-file" type="file" className="hidden" />
           </label>
         </div>
       </div>
     </div>
-
     </>
   )
 }
